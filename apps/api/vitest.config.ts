dbc033f32b8433083cca8b9a043ee2fc1a618ddd
Yann Lojewski <yann.lojewski@gmail.com>
2025-10-30 13:21:11 +0100

    feat(api): add path aliases

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,21 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'vitest/config'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@test': resolve(__dirname, 'test')
+    }
+  },
+  test: {
+    env: {
+      NODE_ENV: 'test'
+    }
+  }
+})
